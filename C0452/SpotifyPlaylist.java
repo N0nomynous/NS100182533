e38a85d595e65b54282cd2c56b77af059caf9955
N0nomynous <145754875+N0nomynous@users.noreply.github.com>
2023-11-03 15:57:05 +0000

    Update SpotifyPlaylist.java

--- C0452/SpotifyPlaylist.java
+++ C0452/SpotifyPlaylist.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-class Song {  //Class created called "Song"
+class Song {
     private String title;
     private String artist;
     private double plays; // Number of times played in millions
@@ -35,7 +35,7 @@ class Song {  //Class created called "Song"
     }
 }
 
-public class SpotifyPlaylist {  //Class created called "SpotifyPlaylist"
+public class SpotifyPlaylist {
     private List<Song> playlist;
 
     public SpotifyPlaylist() {
@@ -68,7 +68,7 @@ public class SpotifyPlaylist {  //Class created called "SpotifyPlaylist"
         }
         if (songToRemove != null) {
             playlist.remove(songToRemove);
-            System.out.println("Song has been removed from the playlist.");
+            System.out.println("Song removed from the playlist.");
         } else {
             System.out.println("Song not found in the playlist.");
         }
@@ -103,7 +103,7 @@ public class SpotifyPlaylist {  //Class created called "SpotifyPlaylist"
         Scanner scanner = new Scanner(System.in);
         int choice;
 
-        do {  //Creating the menu for the user here
+        do {
             System.out.println("\nMenu:");
             System.out.println("1. Add a Song");
             System.out.println("2. Remove a Song");
@@ -116,42 +116,42 @@ public class SpotifyPlaylist {  //Class created called "SpotifyPlaylist"
             scanner.nextLine();
 
             switch (choice) {
-                case 1:  //first option
-                    // Adds a new song with user input
-                    System.out.print("Enter the song title: ");
+                case 1:
+                    // Add a new song with user input
+                    System.out.print("Enter song title: ");
                     String title = scanner.nextLine();
-                    System.out.print("Enter the artist: ");
+                    System.out.print("Enter artist: ");
                     String artist = scanner.nextLine();
                     double plays;
                     while (true) {
-                        System.out.print("Enter the play count (millions): ");
+                        System.out.print("Enter play count (millions): ");
                         if (scanner.hasNextDouble()) {
                             plays = scanner.nextDouble();
                             scanner.nextLine();
                             break;
                         } else {
-                            System.out.println("Invalid input, please enter a valid number.");
+                            System.out.println("Invalid input. Please enter a valid number.");
                             scanner.nextLine(); // Clear the invalid input
                         }
                     }
                     playlistManager.addSong(title, artist, plays);
-                    System.out.println("The Song has been added to the playlist.");
+                    System.out.println("Song added to the playlist.");
                     break;
-                case 2:  //second option
+                case 2:
                     // Remove a song with user input
-                    System.out.print("Enter the song title to remove: ");
+                    System.out.print("Enter song title to remove: ");
                     String titleToRemove = scanner.nextLine();
                     playlistManager.removeSong(titleToRemove);
                     break;
-                case 3:  //third option
+                case 3:
                     // List all songs
                     playlistManager.listAllSongs();
                     break;
-                case 4:  //fourth option
+                case 4:
                     // List play counts
                     playlistManager.listPlayCounts();
                     break;
-                case 5:  //fifth option
+                case 5:
                     // List songs over a threshold
                     double threshold;
                     while (true) {
@@ -161,22 +161,21 @@ public class SpotifyPlaylist {  //Class created called "SpotifyPlaylist"
                             scanner.nextLine();
                             break;
                         } else {
-                            System.out.println("Invalid input, please enter a valid number.");
+                            System.out.println("Invalid input. Please enter a valid number.");
                             scanner.nextLine(); // Clear the invalid input
                         }
                     }
                     playlistManager.listSongsOverThreshold(threshold);
                     break;
-                case 6:  //sixth option
+                case 6:
                     // Exit the program
-                    System.out.println("Exiting the programme.");
+                    System.out.println("Exiting the program.");
                     break;
                 default:
-                    System.out.println("Invalid choice, please enter a valid option.");
+                    System.out.println("Invalid choice. Please enter a valid option.");
             }
-        } while (choice != 6);  //loop
+        } while (choice != 6);
 
         scanner.close();
     }
 }
-//finish
